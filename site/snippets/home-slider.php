097cdd5d5ea8c624ba0f2156f077da555a26a0eb
TheSmithy <krullsimon205@gmail.com>
2016-09-08 12:02:31 +0200

    Edit Files

--- site/snippets/home-slider.php
+++ site/snippets/home-slider.php
@@ -5,21 +5,21 @@
         <div class="carousel-inner">
             <div class="item active ">
 
-                <img src="assets/img/1.jpg" alt="" />
+                <img src="<?= $page->bgimage()->toFile()->url() ?>" />
                 <div class="carousel-caption">
                     <h1 class="slide-h1 wow bounceInDown" data-wow-duration="1s"><?php echo $page->slidertitle()->kirbytext() ?></h1>
                     <h2 class="slide-h2 wow bounceInLeft" data-wow-duration="2s" data-wow-delay="1s"><?php echo $page->slidertext()->kirbytext() ?></h2>
                 </div>
             </div>
             <div class="item">
-                <img src="assets/img/1.jpg" alt="" />
+                <img src="<?= $page->bgimage()->toFile()->url() ?>" />
                 <div class="carousel-caption">
                     <h1 class="slide-h1 wow bounceInDown" data-wow-duration="2s"><?php echo $page->slidertitle1()->kirbytext() ?></h1>
                     <h2 class="slide-h2 wow bounceInLeft" data-wow-duration="2s" data-wow-delay="1s"><?php echo $page->slidertext1()->kirbytext() ?></h2>
                 </div>
             </div>
             <div class="item">
-                <img src="assets/img/1.jpg" alt="" />
+                <img src="<?= $page->bgimage()->toFile()->url() ?>" />
                 <div class="carousel-caption">
                     <h1 class="slide-h1 wow bounceInDown" data-wow-duration="2s"><?php echo $page->slidertitle2()->kirbytext() ?> </h1>
                     <h2 class="slide-h2 wow bounceInLeft" data-wow-duration="2s" data-wow-delay="1s"><?php echo $page->slidertext2()->kirbytext() ?></h2>
